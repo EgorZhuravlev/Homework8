9fa7c690011f35d5a10c43e2b715884f24b7f852
Egor Zhuravlev <zhuravlevegor@gmail.com>
2022-12-18 12:12:06 +0300

    Example1 added

---
+++ Example1/Program.cs
@@ -0,0 +1,65 @@
+﻿/* Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+ */
+
+int[,] randomArray = CreateRandom2DArray(3, 4);
+Print2DArray(randomArray);
+int[,] rowSort = SortRowAscending(randomArray);
+Print2DArray(rowSort);
+
+
+int[,] CreateRandom2DArray(int rows, int columns)
+{
+    Random random = new Random();
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(-10, 10);
+        }
+    }
+
+    return array;
+}
+
+int[,] SortRowAscending(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int minPosition = j;
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, k] > array[i, minPosition]) minPosition = k;
+            }
+            int tmp = array[i, j];
+            array[i, j] = array[i, minPosition];
+            array[i, minPosition] = tmp;
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}, ");
+        }
+        Console.WriteLine();
+    }
+}
