77032cb76306d170746849308565c6cc464f2b38
Egor Zhuravlev <zhuravlevegor@gmail.com>
2022-12-18 14:29:30 +0300

    Example4 added

---
+++ Example4/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, 
+добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1) */
+
+int[,,] randomArray = CreateRandom2DArray1(2, 2, 2);
+Print3DArray(randomArray);
+
+int[,,] CreateRandom2DArray1(int x, int y, int z)
+{
+    Random random = new Random();
+    int[,,] array = new int[x, y, z];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = random.Next(1, 10);
+            }
+        }
+    }
+
+    return array;
+}
+
+void Print3DArray(int[,,] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
